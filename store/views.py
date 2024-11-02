65ce62ea0174363d09ba18140d366f38ad63503b
Munuri-Gangadhar <munurigangadhar0987@gmail.com>
2024-11-02 07:51:33 +0530

    updated

--- store/views.py
+++ store/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render,redirect
-from . models import Product,Category
+from . models import Product,Category,Profile
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from . forms import SignUpForm
+from . forms import SignUpForm,UpdateUserForm,ChangePasswordForm,UserInfoForm
 
 
 
@@ -74,9 +74,73 @@ def register_user(request):
             user=authenticate(username=username,password=password)
             login(request,user)
             messages.success(request,("You have Registered Successfully"))
-            return redirect('home')
+            return redirect('update_user')
         else:
             messages.success(request,("Whoops! There was an problem occured"))
             return redirect('register')
     else:
         return render(request,'register.html',{'form':form})
+
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user=User.objects.get(id=request.user.id)
+        user_form=UpdateUserForm(request.POST or None,instance=current_user)
+
+        if user_form.is_valid():
+            user_form.save()
+            
+            login(request,current_user)
+
+            messages.success(request,"User Has Been Updated")
+            return redirect('home')
+        return render(request,"update_user.html",{'user_form':user_form})
+    
+    else:
+        messages.success(request,"You Must be logged in to Updated")
+        return redirect('home')
+
+def update_password(request):
+    if request.user.is_authenticated:
+        current_user = request.user
+        
+        if request.method == 'POST':
+            form = ChangePasswordForm(current_user, request.POST)
+
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(request, form.user)  # Keeps user logged in after password change
+                messages.success(request, "Your password has been changed.")
+                return redirect('login')
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request, error)
+                    return redirect('update_password')
+        else:
+            form = ChangePasswordForm(current_user)
+        
+        return render(request, 'update_password.html', {'form': form})
+    
+    else:
+        messages.error(request, "You must be logged in to update your password.")
+        return redirect('home')
+
+
+def update_info(request):
+    if request.user.is_authenticated:
+        current_user=Profile.objects.get(user__id=request.user.id)
+        form=UserInfoForm(request.POST or None,instance=current_user)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request,"User Info Has Been Updated")
+            return redirect('home')
+        return render(request,"update_info.html",{'form':form})
+    
+    else:
+        messages.success(request,"You Must be logged in to Updated")
+        return redirect('home')
+
+
+def search(request):
+    pass
