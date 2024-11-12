55b536abf50a768a9d9823eeeb87e0e9fc96bef1
Munuri-Gangadhar <munurigangadhar0987@gmail.com>
2024-11-12 18:39:42 +0530

    Shipping cart

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect
 from cart.cart import Cart
 from . forms import ShippingForm,PaymentForm
-from . models import ShippingAddress
+from . models import ShippingAddress,Order,OrderItem
 from django.contrib import messages
+from django.contrib.auth.models  import User
+from store.models import Product
+
 # Create your views here.
 def payment_success(request):
     return render(request,'payment/payment_success.html',{})
@@ -45,11 +48,67 @@ def billing_info(request):
 
 def process_order(request):
     if request.POST:
+        cart=Cart(request)
+        cart_products=cart.get_prods
+        quantities=cart.get_quants
+        totals=cart.cart_total()
+
         payment_form=PaymentForm(request.POST or None)
         my_shipping=request.session.get('my_shipping')
-        
-        shipping_address=f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}\n"
 
+        
+        full_name=my_shipping['shipping_full_name']
+        email=my_shipping['shipping_email']
+        shipping_address=f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}\n"
+        amount_paid=totals
+
+        if request.user.is_authenticated:
+            user=request.user
+            create_order=Order(user=user,full_name=full_name,email=email,shipping_address=shipping_address,amount_paid=amount_paid)
+            create_order.save()
+
+            order_id=create_order.pk
+
+            for product in cart_products():
+                product_id=product.id
+                if product.is_sale:
+                    price=product.sale_price
+                else:
+                    price=product.price
+                
+                for key,value in quantities().items():
+                    if int(key) == product.id:
+                        create_order_item=OrderItem(order_id=order_id,product_id=product_id,user=user,quantity=value,price=price)
+                        create_order_item.save()
+            for key in list(request.session.keys()):
+                if key=='session_key':
+                    del request.session[key]
+
+
+            messages.success(request,"Order Placed")
+            return redirect('home')
+        else:
+            create_order=Order(full_name=full_name,email=email,shipping_address=shipping_address,amount_paid=amount_paid)
+            create_order.save() 
+            order_id=create_order.pk
+
+            for product in cart_products():
+                product_id=product.id
+                if product.is_sale:
+                    price=product.sale_price
+                else:
+                    price=product.price
+                
+                for key,value in quantities().items():
+                    if int(key) == product.id:
+                        create_order_item=OrderItem(order_id=order_id,product_id=product_id,quantity=value,price=price)
+                        create_order_item.save()
+
+            for key in list(request.session.keys()):
+                 if key=='session_key':
+                    del request.session[key]
+            messages.success(request,"Order Placed")
+            return redirect('home')
 
 
         messages.success(request,"Order Placed")
